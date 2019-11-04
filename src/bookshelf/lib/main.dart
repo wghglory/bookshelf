fcf90468129c95a264e127c9b6e4d34ae72448e2
Kael <kaelsunstrider98@outlook.com>
2019-11-04 18:08:37 +0800

    Initialize App

---
+++ src/bookshelf/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:bookshelf/login/login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bookshelf',
+      theme: new ThemeData.light(),
+      initialRoute: '/',
+      
+      routes: {
+        // When navigating to the "/" route, build the LoginPage widget.
+        '/': (context) => LoginPage(),
+        // When navigating to the "/home" route, build the HomePage widget.
+        //'/home': (context) => HomePage(),
+        //'/bucket':(context) => BucketPage(),
+      },
+      
+    );
+  }
+}
