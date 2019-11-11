acddebc22a6c1e053d142299e150e07f4bb81558
Weijie_Li <>
2019-11-12 00:42:58 +0800

    Add online txt viewer page & enable scrollable page

--- src/online_viewer/lib/main.dart
+++ src/online_viewer/lib/main.dart
@@ -43,11 +43,11 @@ class _PdfViewState extends State<PdfView> {
       Future getfileFromAssets() async {
         //获取file中的数据
         StreamController<Uint8List> controller = StreamController<Uint8List>();
-        String token = "token=yiwei@jiaotong:jiaotong";
+        String token = "token=weijie@jiaotong:jiaotong";
         String method = "&response-content-disposition=inline";
-        String filename = "test.txt";
+        String filename = "Wiley.Algorithms.for.Image.Processing.and.Computer.Vision.2nd.Edition.pdf";
         String uri_filename = Uri.encodeComponent(filename);
-        final url = "http://yhzzzz.natapp1.cc/api/v1/s3/test2/" + uri_filename +
+        final url = "http://yhzzzz.natapp1.cc/api/v1/s3/weijie/" + uri_filename +
             "?" + token + method;
         var request = await HttpClient().getUrl(Uri.parse(url));
         var response = await request.close();
@@ -55,6 +55,7 @@ class _PdfViewState extends State<PdfView> {
         String dir = (await getApplicationDocumentsDirectory()).path;
         File file = new File('$dir/$filename');
         type = filename.substring(filename.lastIndexOf(".") + 1);
+        assetpath = file.path;
 
         //pdf 写入
         if (type == "pdf") {
@@ -63,12 +64,15 @@ class _PdfViewState extends State<PdfView> {
           });
           controller.sink.add(bytes);
           controller.close();
-          assetpath = file.path;
+
           //return file;
         }
 
         //txt 写入
         else if(type=="txt"){
+          print("1");
+          await file.writeAsBytes(bytes);
+          print("2");
           String contents=await file.readAsString();
           allText=contents;
         }
@@ -85,7 +89,7 @@ class _PdfViewState extends State<PdfView> {
             onPressed: () {
               getfileFromAssets();
               //print("BUG");
-              if(assetpath==""){
+              if(assetpath=="" || allText==""){
                 print("waiting");
               }
               else if(type=="pdf"){
@@ -96,12 +100,9 @@ class _PdfViewState extends State<PdfView> {
               }
               else if(type=="txt"){
                 print("txt!!!!!!!");
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Container(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Text(allText),
-                  ),),
+                Navigator.push(context, MaterialPageRoute(
+                      builder: (context) => TxtPage(content: allText),
+                  )
                 );
               }
             },
@@ -157,6 +158,55 @@ class _PdfPageState extends State<PdfPage> {
               : Offstage()
         ],
       ),
+    );
+  }
+}
+
+
+class TxtPage extends StatefulWidget {
+  String content="";
+
+  TxtPage({Key key, this.content}) : super(key: key);
+
+  @override
+  _TxtPageState createState() => _TxtPageState();
+}
+
+class _TxtPageState extends State<TxtPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Txt"),
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new Text(
+            widget.content,
+            textAlign: TextAlign.left, //文本对齐方式  居中
+            textDirection: TextDirection.ltr, //文本方向
+            softWrap: true, //是否自动换行 false文字不考虑容器大小  单行显示   超出；屏幕部分将默认截断处理
+            overflow: TextOverflow.fade,
+            textScaleFactor: 2.0, //字体显示的赔率
+            //maxLines: , //最大行数
+            style: new TextStyle(
+              decorationColor: const Color(0xffffffff), //线的颜色
+              decoration: TextDecoration.none,
+              decorationStyle: TextDecorationStyle.solid, //文字装饰的风格  dashed,dotted虚线(简短间隔大小区分)  double三条线  solid两条线
+              wordSpacing: 0.0, //单词间隙(负值可以让单词更紧凑)
+              letterSpacing: 0.0, //字母间隙(负值可以让字母更紧凑)
+              fontStyle: FontStyle.normal, //文字样式，斜体和正常
+              fontSize: 12.0, //字体大小
+              fontWeight: FontWeight.normal, //字体粗细  粗体和正常
+              color: Colors.black, //文字颜色
+            ),
+          ),
+
+        ],
+
+          ),
     );
   }
 }
